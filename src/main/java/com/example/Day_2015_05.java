b43bb09ca64025850d035d142dd6520af8964d2f
linventif <gregoire.becue@proton.me>
2025-09-11 11:59:37 +0200

    feat: add day2015-05 data

--- src/main/java/com/example/Day_2015_05.java
+++ src/main/java/com/example/Day_2015_05.java
@@ -7,30 +7,31 @@ import java.util.Scanner;
 
 /**
  *
- * --- Day 5: Perfectly Spherical Houses in a Vacuum ---
+ * -- Day 5: Doesn't He Have Intern-Elves For This? ---
  *
- * Santa is delivering presents to an infinite two-dimensional grid of houses.
+ * Santa needs help figuring out which strings in his text file are naughty or
+ * nice.
  *
- * He begins by delivering a present to the house at his starting location, and
- * then an elf at the North Pole calls him via radio and tells him where to move
- * next. Moves are always exactly one house to the north (^), south (v), east
- * (>), or west (<). After each move, he delivers another present to the house
- * at his new location.
+ * A nice string is one with all of the following properties:
  *
- * However, the elf back at the north pole has had a little too much eggnog, and
- * so his directions are a little off, and Santa ends up visiting some houses
- * more than once. How many houses receive at least one present?
+ * It contains at least three vowels (aeiou only), like aei, xazegov, or
+ * aeiouaeiouaeiou.
+ * It contains at least one letter that appears twice in a row, like xx, abcdde
+ * (dd), or aabbccdd (aa, bb, cc, or dd).
+ * It does not contain the strings ab, cd, pq, or xy, even if they are part of
+ * one of the other requirements.
  *
  * For example:
  *
- * > delivers presents to 2 houses: one at the starting location, and one to the
- * east.
- * ^>v< delivers presents to 4 houses in a square, including twice to the house
- * at his starting/ending location.
- * ^v^v^v^v^v delivers a bunch of presents to some very lucky children at only 2
- * houses.
+ * ugknbfddgicrmopn is nice because it has at least three vowels (u...i...o...),
+ * a double letter (...dd...), and none of the disallowed substrings.
+ * aaa is nice because it has at least three vowels and a double letter, even
+ * though the letters used by different rules overlap.
+ * jchzalrnumimnmhp is naughty because it has no double letter.
+ * haegwjzuvuyypxyu is naughty because it contains the string xy.
+ * dvszwmarrgswjxmb is naughty because it contains only one vowel.
  *
- *
+ * How many strings are nice?
  */
 public class Day_2015_05 {
     private static final String PATH_DATA = "src" + File.separator + "data" + File.separator;
