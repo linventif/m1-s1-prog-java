41f5419b35aad10da95898c5f0edf4a5f2502a27
linventif <gregoire.becue@proton.me>
2025-09-11 10:43:03 +0200

    feat: add day 2015-02

--- src/main/java/com/example/Day_2015_03.java
+++ src/main/java/com/example/Day_2015_03.java
@@ -3,6 +3,7 @@ package com.example;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -34,47 +35,44 @@ import java.util.Scanner;
  */
 public class Day_2015_03 {
     private static final String PATH_DATA = "src" + File.separator + "data" + File.separator;
-    public static int sqFeet;
-    public static int riFeet;
+    public static int[][] pos = { { 0, 0 } };
+    public static HashMap<String, Integer> alreadyVisited = new HashMap<>();
 
     public static void main(String[] args) {
-        String fichier = "Day_2015_02.txt";
+        String fichier = "Day_2015_03.txt";
+        alreadyVisited.put("0,0", 1);
         try (Scanner scanner = new Scanner(new FileInputStream(PATH_DATA + fichier))) {
             while (scanner.hasNextLine()) {
                 String ch = scanner.nextLine();
                 ch = ch.trim();
 
                 if (!ch.isEmpty()) {
-                    String[] parts = ch.split("x");
+                    for (int i = 0; i < ch.length(); i++) {
+                        char chr = ch.charAt(i);
+                        if (chr == '^') {
+                            pos[0][0] += 1;
+                        } else if (chr == 'v') {
+                            pos[0][0] -= 1;
+                        } else if (chr == '<') {
+                            pos[0][1] -= 1;
+                        } else if (chr == '>') {
+                            pos[0][1] += 1;
+                        }
 
-                    int[] tbl = new int[parts.length];
-                    for (int i = 0; i < parts.length; i++) {
-                        tbl[i] = Integer.parseInt(parts[i]);
+                        System.out.println("pos: " + pos[0][0] + "," + pos[0][1]);
+                        String key = pos[0][0] + "," + pos[0][1];
+                        if (alreadyVisited.containsKey(key)) {
+                            alreadyVisited.put(key, alreadyVisited.get(key) + 1);
+                        } else {
+                            alreadyVisited.put(key, 1);
+                        }
                     }
-
-                    int[] tblRtn = new int[parts.length];
-
-                    // 2*l*w + 2*w*h + 2*h*l
-                    // [l,w,h]
-                    tblRtn[0] = tbl[0] * tbl[1];
-                    tblRtn[1] = tbl[1] * tbl[2];
-                    tblRtn[2] = tbl[2] * tbl[0];
-
-                    int extra = Math.min(tblRtn[0], Math.min(tblRtn[1], tblRtn[2]));
-                    int max = Math.max(tblRtn[0], Math.max(tblRtn[1], tblRtn[2]));
-                    System.out.println("" + max + ":" + "");
-                    sqFeet += 2 * tblRtn[0] + 2 * tblRtn[1] + 2 * tblRtn[2] + extra;
-
-                    // cube ribon
-                    // riFeet += (tbl[0] + tbl[1] + tbl[2] - max) * 2;
-                    riFeet += tbl[0] * tbl[1] + tbl[2];
                 }
             }
         } catch (FileNotFoundException fne) {
             System.err.println(fichier + " is missing ! ");
         }
 
-        System.out.println("Total square feet of paper: " + sqFeet);
-        System.out.println("Total square feet of rub: " + riFeet);
+        System.out.println("result: " + alreadyVisited.size());
     }
 }
